5fa5fb23b19f5067c497cdcc861dea1ec3918392
kazirafi71 <kazirafi66@gmail.com>
2020-10-19 19:55:29 +0600

    Updates

---
+++ precache-manifest.63074cca8140a46177ea9b36397ebc91.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a8b0ba22186075aa391ba61b4a360f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5c700d65cc2addcdfde8",
+    "url": "/Portfolio/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "543b1e4fcc82b2977658",
+    "url": "/Portfolio/static/css/main.06275974.chunk.css"
+  },
+  {
+    "revision": "5c700d65cc2addcdfde8",
+    "url": "/Portfolio/static/js/2.25129f00.chunk.js"
+  },
+  {
+    "revision": "f646b7d433c14d3ca5cca1fdd77b4a81",
+    "url": "/Portfolio/static/js/2.25129f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543b1e4fcc82b2977658",
+    "url": "/Portfolio/static/js/main.dd7816d0.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "7f2fc85922aa6569ab5814377f233e46",
+    "url": "/Portfolio/static/media/72057312_2532004303700525_1511605660993716224_o.7f2fc859.jpg"
+  },
+  {
+    "revision": "dfa451b99be90167480a3b89a6c227f7",
+    "url": "/Portfolio/static/media/87626630_2617805838453704_2702793984178126848_o (1).dfa451b9.jpg"
+  },
+  {
+    "revision": "f8673428a6dce6d73cf9dd8aa561c95e",
+    "url": "/Portfolio/static/media/pexels-francesco-ungaro-673648.f8673428.jpg"
+  },
+  {
+    "revision": "50b6586f983a5f33ef56bd1f52f79b32",
+    "url": "/Portfolio/static/media/pexels-iván-rivero-1478259.50b6586f.jpg"
+  },
+  {
+    "revision": "585e4dd31ac05980dfd1f1895931021b",
+    "url": "/Portfolio/static/media/pexels-kaboompics-com-6444.585e4dd3.jpg"
+  },
+  {
+    "revision": "74edfbe9d00098942ae87859c66f2873",
+    "url": "/Portfolio/static/media/pexels-lukas-590037.74edfbe9.jpg"
+  },
+  {
+    "revision": "49597143c48314f2a4c216655d9463a8",
+    "url": "/Portfolio/static/media/pexels-murtaza-saifee-1684617.49597143.jpg"
+  },
+  {
+    "revision": "09a2a8d52da9d302cb32bc49e2012e9a",
+    "url": "/Portfolio/static/media/pexels-olia-danilevich-4974915.09a2a8d5.jpg"
+  }
+]);
