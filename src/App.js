d65ae50a8b7302584179f1beee0af858e1ce3543
kazirafi71 <kazirafi66@gmail.com>
2021-03-13 14:55:42 +0600

    edit

--- src/App.js
+++ src/App.js
@@ -40,12 +40,12 @@ function App() {
   let [loading, setLoading] = useState(false);
   let [color, setColor] = useState("orange");
 
-  // useEffect(() => {
-  //   setLoading(true)
-  //   setTimeout(() => {
-  //     setLoading(false)
-  //   }, 2000)
-  // }, [])
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  }, [])
 
   return (
     
